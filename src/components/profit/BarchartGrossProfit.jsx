a30ba0d192676a9c7e4647769143848e52d8c777
Lachitha <it21019586@my.sliit.lk>
2023-09-08 20:59:37 +0530

    implement Linecharts

--- src/components/profit/BarchartGrossProfit.jsx
+++ src/components/profit/BarchartGrossProfit.jsx
@@ -12,56 +12,56 @@ import {
 
 const data = [
   {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
+    name: "2017",
+    PresentYear: 4000,
+    PreviousYear: 2400,
     amt: 2400,
   },
   {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
+    name: "2018",
+    PresentYear: 3000,
+    PreviousYear: 1398,
     amt: 2210,
   },
   {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
+    name: "2019",
+    PresentYear: 2000,
+    PreviousYear: 9800,
     amt: 2290,
   },
   {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
+    name: "2020",
+    PresentYear: 2780,
+    PreviousYear: 3908,
     amt: 2000,
   },
   {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
+    name: "2021",
+    PresentYear: 1890,
+    PreviousYear: 4800,
     amt: 2181,
   },
   {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
+    name: "2022",
+    PresentYear: 2390,
+    PreviousYear: 3800,
     amt: 2500,
   },
   {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
+    name: "2023",
+    PresentYear: 3490,
+    PreviousYear: 4300,
     amt: 2100,
   },
 ];
 
 function Barchart() {
   return (
-    <div className="ageBarChart">
+    <div className="netProfit">
       <h1 className="mb-5 text-3xl font-bold " style={{ color: "#494FBF" }}>
-        Age Segmentation
+      Gross Profit
       </h1>
-      <div className="ageChart">
+      <div className="netProfit">
         <ResponsiveContainer width={250} height={200}>
           <BarChart data={data}>
             <CartesianGrid strokeDasharray="3 3" />
@@ -69,8 +69,8 @@ function Barchart() {
             <YAxis />
             <Tooltip />
             <Legend />
-            <Bar dataKey="pv" fill="#EC5959" />
-            <Bar dataKey="uv" fill="#16BFD6" />
+            <Bar dataKey="PreviousYear" fill="#EC5959" />
+            <Bar dataKey="PresentYear" fill="#16BFD6" />
           </BarChart>
         </ResponsiveContainer>
       </div>
--- src/pages/profit/Profit.jsx
+++ src/pages/profit/Profit.jsx
@@ -1,121 +1,91 @@
 import React from "react";
 import { FaRegCalendarMinus } from "react-icons/fa";
 import { Link } from "react-router-dom";
-import Barchart from '/src/pages/profit/Barchart.jsx';
+import Barchart from "../../components/profit/BarchartGrossProfit";
+import BarchartNetProfit from "../../components/profit/BarchartNetProfit";
+import ProductTable from "../../components/profit/ProductTable";
+import LineChartProfit from "../../components/profit/LineChartGrossProfit";
+import LineChartNetProfit from "../../components/profit/LineChartNetProfit";
 
 const Profit = () => {
-  // Sample table data
-  const tableData = [
-    { id: 1, name: "Product A", revenue: 5000 },
-    { id: 2, name: "Product B", revenue: 7500 },
-    { id: 3, name: "Product C", revenue: 10000 },
-    { id: 4, name: "Product D", revenue: 3000 },
-    { id: 5, name: "Product E", revenue: 6000 },
-    // ... (repeated data)
-  ];
+	return (
+		<div className="py-8 px-4 sm:px-8 bg-white">
+			<div className="container mx-auto">
+				{/* Title */}
+				{/* <div className="bg-white py-8 px-4 sm:px-8 rounded-lg shadow-lg text-center">
+					<h1 className="text-xl md:text-5xl font-semibold text-blue-900 leading-tight cursor-pointer">
+						PROFIT PREDICTION
+					</h1>
+				</div> */}
 
-  return (
-    <div className="py-8 px-4 sm:px-8 bg-white">
-      <div className="container mx-auto">
-        {/* Title */}
-        <h1 className="text-4xl md:text-5xl font-semibold text-blue-900 leading-tight cursor-pointer">
-          PROFIT PREDICTION
-        </h1>
+				{/* Grid for Cards */}
+				<div className="grid md:grid-cols-3 ">
+					{/* Card 1: Profit Prediction */}
+					<div className="bg-white rounded-lg p-6 text-center shadow-md">
+						<h2 className="text-xl font-semibold text-blue-900">
+							Profit Prediction (Monthly)
+						</h2>
+						<h1 className="text-3xl font-bold text-blue-700 mt-4">
+							Rs. 100,000
+						</h1>
+						<FaRegCalendarMinus className="text-4xl mt-4 text-blue-600" />
+					</div>
 
-        {/* Grid for Cards */}
-        <div className="grid md:grid-cols-3 ">
-          {/* Card 1: Profit Prediction */}
-          <div className="bg-white rounded-lg p-6 text-center shadow-md">
-            <h2 className="text-xl font-semibold text-blue-900">
-              Profit Prediction (Monthly)
-            </h2>
-            <h1 className="text-3xl font-bold text-blue-700 mt-4">
-              Rs. 100,000
-            </h1>
-            <FaRegCalendarMinus className="text-4xl mt-4 text-blue-600" />
-          </div>
+					{/* Card 2: No. of Products */}
+					<div className="bg-white rounded-lg p-6 text-center shadow-md">
+						<h2 className="text-xl font-semibold text-blue-900">
+							No. of Products
+						</h2>
+						<h1 className="text-3xl font-bold text-blue-700 mt-4">100,000</h1>
+					</div>
 
-          {/* Card 2: No. of Products */}
-          <div className="bg-white rounded-lg p-6 text-center shadow-md">
-            <h2 className="text-xl font-semibold text-blue-900">
-              No. of Products
-            </h2>
-            <h1 className="text-3xl font-bold text-blue-700 mt-4">
-              100,000
-            </h1>
-          </div>
+					{/* Card 3: No. of Products */}
+					<div className="bg-white rounded-lg p-6 text-center shadow-md">
+						<h2 className="text-xl font-semibold text-blue-900">
+							No. of Products
+						</h2>
+						<h1 className="text-3xl font-bold text-blue-700 mt-4">100,000</h1>
+					</div>
+				</div>
 
-          {/* Card 3: No. of Products */}
-          <div className="bg-white rounded-lg p-6 text-center shadow-md">
-            <h2 className="text-xl font-semibold text-blue-900">
-              No. of Products
-            </h2>
-            <h1 className="text-3xl font-bold text-blue-700 mt-4">
-              100,000
-            </h1>
-          </div>
-        </div>
+				{/* Charts and Table */}
+				<div className="flex mt-5">
+					{/* Chart 1 */}
+					<div className="w-1/4 bg-white rounded-lg p-6 text-center shadow-md">
+						<Barchart />
+					</div>
 
-        {/* Table */}
-        <div className="flex mt-5">
-          {/* Chart 1 */}
-          <div className="w-1/4 bg-white rounded-lg p-6 text-center shadow-md">
-            <Barchart />
-          </div>
+					{/* Chart 2 */}
+					<div className="w-1/4 bg-white rounded-lg p-6 text-center shadow-md">
+						<BarchartNetProfit />
+					</div>
 
-          {/* Chart 2 */}
-          <div className="w-1/4 bg-white rounded-lg p-6 text-center shadow-md">
-            <Barchart />
-          </div>
+					{/* Table */}
+					<div className="w-1/2 bg-white rounded-lg shadow-lg p-4">
+						<h3
+							className="mb-5 text-3xl font-bold text-center"
+							style={{ color: "#494FBF" }}>
+							Product Table
+						</h3>
 
-          {/* Table */}
-          <div className="w-1/2 bg-white rounded-lg shadow-lg p-4">
-            <div className="container mx-auto mt-4 p-2 border border-gray-300 rounded-lg max-h-[300px] overflow-y-auto">
-              <div className="table-wrapper">
-                <table className="min-w-full">
-                  <thead>
-                    <tr>
-                      <th className="text-left px-6 py-3 bg-gray-200">ID</th>
-                      <th className="text-left px-6 py-3 bg-gray-200">
-                        Product Name
-                      </th>
-                      <th className="text-left px-6 py-3 bg-gray-200">
-                        Revenue
-                      </th>
-                    </tr>
-                  </thead>
-                </table>
-              </div>
-              <div className="table-scroll">
-                <table className="min-w-full">
-                  <tbody>
-                    {tableData.map((item) => (
-                      <tr key={item.id}>
-                        <td className="text-left px-6 py-3">
-                          <Link to={`/product/${item.id}`}>{item.id}</Link>
-                        </td>
-                        <td className="text-left px-6 py-3">
-                          <Link to={`/product/${item.id}`}>{item.name}</Link>
-                        </td>
-                        <td className="text-left px-6 py-3">
-                          <Link to={`/product/${item.id}`}>
-                            Rs. {item.revenue}
-                          </Link>
-                        </td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="flex mt-5">
-        <h1>1</h1>
-        </div>
-    </div>
-  );
+						<div className="container mx-auto mt-4 p-2 border border-gray-300 rounded-lg max-h-[250px] overflow-y-auto">
+							<ProductTable />
+						</div>
+					</div>
+				</div>
+			</div>
+
+			{/* Additional Content */}
+			<div className="flex mt-5">
+				<div className="w-2/4 bg-white rounded-lg p-6 text-center shadow-md">
+					<LineChartProfit />
+				</div>
+				<div className="w-2/4 bg-white rounded-lg p-6 text-center shadow-md">
+					<LineChartNetProfit />
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Profit;
