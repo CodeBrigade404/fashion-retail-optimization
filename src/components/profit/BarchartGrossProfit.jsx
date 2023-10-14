32989501977345aa5842b941d1976267a3742d3a
Lachitha <it21019586@my.sliit.lk>
2023-10-14 23:34:52 +0530

    backend api connect

--- src/components/profit/BarchartGrossProfit.jsx
+++ src/components/profit/BarchartGrossProfit.jsx
@@ -1,7 +1,6 @@
 import {
 	BarChart,
 	Bar,
-	Cell,
 	XAxis,
 	YAxis,
 	CartesianGrid,
@@ -9,56 +8,40 @@ import {
 	Legend,
 	ResponsiveContainer,
 } from "recharts";
-
-const data = [
-	{
-		name: "2017",
-		PresentYear: 4000,
-		PreviousYear: 2400,
-		amt: 2400,
-	},
-	{
-		name: "2018",
-		PresentYear: 3000,
-		PreviousYear: 1398,
-		amt: 2210,
-	},
-	{
-		name: "2019",
-		PresentYear: 2000,
-		PreviousYear: 9800,
-		amt: 2290,
-	},
-	{
-		name: "2020",
-		PresentYear: 2780,
-		PreviousYear: 3908,
-		amt: 2000,
-	},
-	{
-		name: "2021",
-		PresentYear: 1890,
-		PreviousYear: 4800,
-		amt: 2181,
-	},
-	{
-		name: "2022",
-		PresentYear: 2390,
-		PreviousYear: 3800,
-		amt: 2500,
-	},
-	{
-		name: "2023",
-		PresentYear: 3490,
-		PreviousYear: 4300,
-		amt: 2100,
-	},
-];
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function Barchart() {
+	const [data, setData] = useState([]);
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		axios
+			.get("http://localhost:3001/sales/calculateSalesAndProfitByYear")
+			.then((response) => {
+				setData(response.data.salesByYear);
+				setLoading(false);
+				console.log("Response Data:", response.data);
+			})
+			.catch((error) => {
+				console.error("Error fetching data: ", error);
+				setLoading(false);
+			});
+	}, []);
+
+	if (loading) {
+		return <div>Loading...</div>;
+	}
+
+	if (data.length === 0) {
+		return <div>No data available.</div>;
+	}
+
 	return (
 		<div className="netProfit">
-			<h2 className="text-left  ml-4 font-tinos text-2xl">Gross Profit</h2>
+			<h2 className="text-left ml-4 font-tinos text-2xl">
+				Gross Profit - Yearly
+			</h2>
 			<div className="netProfit">
 				<ResponsiveContainer width={550} height={200}>
 					<BarChart data={data}>
@@ -67,8 +50,8 @@ function Barchart() {
 						<YAxis />
 						<Tooltip />
 						<Legend />
-						<Bar dataKey="PreviousYear" fill="#cbd5e1" />
-						<Bar dataKey="PresentYear" fill="#64748b" />
+						<Bar dataKey="totalSales" fill="#cbd5e1" />
+						<Bar dataKey="totalProfit" fill="#64748b" />
 					</BarChart>
 				</ResponsiveContainer>
 			</div>
