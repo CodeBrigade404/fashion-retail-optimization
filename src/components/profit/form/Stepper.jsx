76d917acab5803035f233c842f6a48afcdc125a6
ManulaK <it21019968@my.sliit.lk>
2023-10-15 08:41:54 +0530

    profit from added

---
+++ src/components/profit/form/Stepper.jsx
@@ -0,0 +1,24 @@
+import Box from "@mui/material/Box";
+import Stepper from "@mui/material/Stepper";
+import Step from "@mui/material/Step";
+import StepLabel from "@mui/material/StepLabel";
+
+const steps = [
+  "Add General product Information",
+  "Authorized Product",
+  "Finalized Product",
+];
+
+export default function HorizontalLinearAlternativeLabelStepper() {
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Stepper activeStep={1} alternativeLabel>
+        {steps.map((label) => (
+          <Step key={label}>
+            <StepLabel>{label}</StepLabel>
+          </Step>
+        ))}
+      </Stepper>
+    </Box>
+  );
+}
